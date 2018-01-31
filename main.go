cd03f5294e232662fd94bcffae63ad4f8f2c3970
hwchiu <hwchiu@linkernetworks.com>
2018-01-31 18:37:02 +0800

    move the scan function to fillutils

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bitbucket.org/linkernetworks/aurora/src/utils/fileutils"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"io/ioutil"
@@ -8,42 +9,13 @@ import (
 	"mime"
 	"net/http"
 	"path"
-	"time"
 )
-
-type FileInfo struct {
-	Name    string    `json:"name"`
-	Size    int64     `json:"size"`
-	Type    string    `json:"type"`
-	ModTime time.Time `json:"mtime"`
-	IsDir   bool      `json:"isDir"`
-}
 
 type FileContent struct {
 	Name    string `json:"name"`
 	Ext     string `json:"ext"`
 	Type    string `json:"type"`
 	Content []byte `json:"content"`
-}
-
-func ScanDir(p string) ([]FileInfo, error) {
-	fileInfos := []FileInfo{}
-	files, err := ioutil.ReadDir(p)
-	if err != nil {
-		return fileInfos, err
-	}
-
-	for _, file := range files {
-		fileInfos = append(fileInfos, FileInfo{
-			Name:    file.Name(),
-			Size:    file.Size(),
-			ModTime: file.ModTime(),
-			IsDir:   file.IsDir(),
-			Type:    mime.TypeByExtension(path.Ext(file.Name())),
-		})
-	}
-
-	return fileInfos, nil
 }
 
 func ReadFileHandler(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +44,7 @@ func ReadFileHandler(w http.ResponseWriter, r *http.Request) {
 func LoadDirHandler(w http.ResponseWriter, r *http.Request) {
 	values := mux.Vars(r)
 
-	infos, err := ScanDir("/" + values["path"])
+	infos, err := fileutils.ScanDir("/" + values["path"])
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusNotFound)
