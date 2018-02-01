46c13f08cb16fa88a2d6dbd89038b211d69487fc
hwchiu <hwchiu@linkernetworks.com>
2018-02-01 15:13:51 +0800

    move the handler to src/fileserver

--- main.go
+++ main.go
@@ -1,130 +1,20 @@
 package main
 
 import (
-	"bitbucket.org/linkernetworks/aurora/src/utils/fileutils"
-	"encoding/json"
+	fs "bitbucket.org/linkernetworks/aurora/src/fileserver"
 	"flag"
 	"github.com/gorilla/mux"
-	"io/ioutil"
-	"log"
-	"mime"
 	"net"
 	"net/http"
-	"os"
-	"path"
 )
-
-const root = "/"
-
-type FileContent struct {
-	Name    string `json:"name"`
-	Ext     string `json:"ext"`
-	Type    string `json:"type"`
-	Content []byte `json:"content"`
-}
-
-func writeError(w http.ResponseWriter, err error, code int) {
-	w.WriteHeader(code)
-	w.Write([]byte(err.Error()))
-}
-
-func RemoveFileHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to delete the file")
-	values := mux.Vars(r)
-	p := root + values["path"]
-
-	log.Println("target path is ", p)
-	if err := os.RemoveAll(p); err != nil {
-		log.Println(err)
-		writeError(w, err, http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	log.Println("Delete file success")
-}
-func WriteFileHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to upload the file")
-	values := mux.Vars(r)
-	p := "/" + values["path"]
-
-	log.Println("target path is ", p)
-	var fc FileContent
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&fc); err != nil {
-		log.Println(err)
-		writeError(w, err, http.StatusInternalServerError)
-		return
-	}
-	defer r.Body.Close()
-
-	filePath := p + "/" + fc.Name
-	if err := ioutil.WriteFile(filePath, fc.Content, 0644); err != nil {
-		log.Println(err)
-		writeError(w, err, http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	log.Println("Upload file success")
-}
-
-func ReadFileHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to read the file")
-	values := mux.Vars(r)
-	p := root + values["path"]
-
-	log.Println("target path is ", p)
-	bytes, err := ioutil.ReadFile(p)
-	if err != nil {
-		log.Println(err)
-		writeError(w, err, http.StatusNotFound)
-		return
-	}
-
-	response, err := json.Marshal(FileContent{
-		Name:    path.Base(p),
-		Ext:     path.Ext(p),
-		Type:    mime.TypeByExtension(path.Ext(p)),
-		Content: bytes,
-	})
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write(response)
-	log.Println("Read file success")
-}
-
-func ScanDirHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to load the dir")
-	values := mux.Vars(r)
-	p := root + values["path"]
-
-	log.Println("target path is ", p)
-	infos, err := fileutils.ScanDir(p)
-	if err != nil {
-		log.Println(err)
-		writeError(w, err, http.StatusNotFound)
-		return
-	}
-
-	response, err := json.Marshal(infos)
-	if err != nil {
-		log.Println(err)
-		writeError(w, err, http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(response)
-	log.Println("Load dir success")
-}
 
 func newRouterServer() http.Handler {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/scan/{path:.*}", ScanDirHandler).Methods("GET")
-	router.HandleFunc("/read/{path:.*}", ReadFileHandler).Methods("GET")
-	router.HandleFunc("/write/{path:.*}", WriteFileHandler).Methods("POST")
-	router.HandleFunc("/delete/{path:.*}", RemoveFileHandler).Methods("DELETE")
+	router.HandleFunc("/scan/{path:.*}", fs.ScanDirHandler).Methods("GET")
+	router.HandleFunc("/read/{path:.*}", fs.ReadFileHandler).Methods("GET")
+	router.HandleFunc("/write/{path:.*}", fs.WriteFileHandler).Methods("POST")
+	router.HandleFunc("/delete/{path:.*}", fs.RemoveFileHandler).Methods("DELETE")
 	return router
 }
 
