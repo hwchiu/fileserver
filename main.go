4c5842b4013ff0819be67c46280400002189c00b
c9s <yoanlin93@gmail.com>
2018-03-28 11:59:35 +0800

    improve file server logging and add basepath document option

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"bitbucket.org/linkernetworks/aurora/src/aurora"
-	fs "bitbucket.org/linkernetworks/aurora/src/fileserver"
 	"flag"
-	"github.com/gorilla/mux"
 	"net"
 	"net/http"
+
+	"bitbucket.org/linkernetworks/aurora/src/aurora"
+	fs "bitbucket.org/linkernetworks/aurora/src/fileserver"
+	"bitbucket.org/linkernetworks/aurora/src/logger"
+
+	"github.com/gorilla/mux"
 )
 
-func newRouterServer() http.Handler {
+func newRouterServer(root string, basepath string) http.Handler {
 	router := mux.NewRouter()
-	root := "/workspace"
+	if len(basepath) > 0 {
+		router = router.PathPrefix(basepath).Subrouter()
+	}
 	router.HandleFunc("/scan/{path:.*}", fs.GetScanDirHandler(root)).Methods("GET")
 	router.HandleFunc("/scan", fs.GetScanDirHandler(root)).Methods("GET")
 	router.HandleFunc("/read/{path:.*}", fs.GetReadFileHandler(root)).Methods("GET")
@@ -23,11 +28,15 @@ func newRouterServer() http.Handler {
 func main() {
 	var host string
 	var port string
+	var documentRoot string
+	var basePath string
 	var version bool = false
 
 	flag.BoolVar(&version, "version", false, "version")
-	flag.StringVar(&host, "h", "", "hostname")
-	flag.StringVar(&port, "p", "33333", "port")
+	flag.StringVar(&documentRoot, "documentRoot", "/workspace", "the document root of the file server")
+	flag.StringVar(&basePath, "basePath", "", "the url base path of the APIs")
+	flag.StringVar(&host, "host", "", "hostname")
+	flag.StringVar(&port, "port", "33333", "port")
 	flag.Parse()
 
 	if version {
@@ -35,8 +44,18 @@ func main() {
 		return
 	}
 
-	bind := net.JoinHostPort(host, port)
+	logger.Infof("Serving document root: %s at %s", documentRoot, basePath)
+	router := newRouterServer(documentRoot, basePath)
 
-	router := newRouterServer()
-	http.ListenAndServe(bind, router)
+	bind := net.JoinHostPort(host, port)
+	logger.Infof("Listening at %s", bind)
+
+	http.ListenAndServe(bind, logRequest(router))
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.Infof("[%s] %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
