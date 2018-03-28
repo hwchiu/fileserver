5e039e8cdeb3dd45bcd4a5f21d5a3e8c23ef371c
c9s <yoanlin93@gmail.com>
2018-03-28 11:59:35 +0800

    improve file server logging and add basepath document option

--- fileserver.go
+++ fileserver.go
@@ -1,11 +1,12 @@
 package fileserver
 
 import (
+	"bitbucket.org/linkernetworks/aurora/src/logger"
 	"bitbucket.org/linkernetworks/aurora/src/utils/fileutils"
+
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"io/ioutil"
-	"log"
 	"mime"
 	"net/http"
 	"os"
@@ -50,31 +51,26 @@ func GetScanDirHandler(root string) func(w http.ResponseWriter, r *http.Request)
 	}
 }
 func RemoveFileHandler(root string, w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to delete the file")
 	values := mux.Vars(r)
 	p := path.Join(root, values["path"])
 
-	log.Println("target path is ", p)
 	if err := os.RemoveAll(p); err != nil {
-		log.Println(err)
+		logger.Errorf("remove error: %v", err)
 		writeError(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	log.Println("Delete file success")
 }
 func WriteFileHandler(root string, w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to upload the file")
 	values := mux.Vars(r)
 
 	p := path.Join(root, values["path"])
 
-	log.Println("target path is ", p)
 	var fc FileContent
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&fc); err != nil {
-		log.Println(err)
+		logger.Errorf("json error: %v", err)
 		writeError(w, err, http.StatusInternalServerError)
 		return
 	}
@@ -82,23 +78,20 @@ func WriteFileHandler(root string, w http.ResponseWriter, r *http.Request) {
 
 	filePath := p + "/" + fc.Name
 	if err := ioutil.WriteFile(filePath, fc.Content, 0644); err != nil {
-		log.Println(err)
+		logger.Errorf("write error: %v", err)
 		writeError(w, err, http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	log.Println("Upload file success")
 }
 
 func ReadFileHandler(root string, w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to read the file")
 	values := mux.Vars(r)
 	p := path.Join(root, values["path"])
 
-	log.Println("target path is ", p)
 	bytes, err := ioutil.ReadFile(p)
 	if err != nil {
-		log.Println(err)
+		logger.Errorf("read error: %v", err)
 		writeError(w, err, http.StatusNotFound)
 		return
 	}
@@ -112,33 +105,29 @@ func ReadFileHandler(root string, w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
-	log.Println("Read file success")
 }
 
 func ScanDirHandler(root string, w http.ResponseWriter, r *http.Request) {
-	log.Println("Ready to load the dir")
 	values := mux.Vars(r)
 	p := root
 	if subPath, ok := values["path"]; ok {
 		p = path.Join(root, subPath)
 	}
 
-	log.Println("target path is ", p)
 	infos, err := fileutils.ScanDir(p)
 	if err != nil {
-		log.Println(err)
+		logger.Errorf("scan dir error: %v", err)
 		writeError(w, err, http.StatusNotFound)
 		return
 	}
 
 	response, err := json.Marshal(infos)
 	if err != nil {
-		log.Println(err)
+		logger.Errorf("json error: %v", err)
 		writeError(w, err, http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
-	log.Println("Load dir success")
 }
